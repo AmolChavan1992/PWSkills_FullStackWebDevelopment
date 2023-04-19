b7846dc192ab66b0c67418d15f2fb09aa9608db1
AmolChavan1992 <chavanaa1992@gmail.com>
2023-04-19 23:03:24 +0530

    Milestone assignment - 12, 13

---
+++ Milestone Assignment/13. Bill Splitter/script.js
@@ -0,0 +1,6 @@
+function bill_splitter(cost_of_each_dish, number_of_people) {
+}
+
+const cost_of_each_dish = 150;
+const number_of_people = 5;
+console.log(bill_splitter(cost_of_each_dish, number_of_people));
